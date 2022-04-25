d0038e9fdb96c62e77a045dae3c13fdd587b1915
vinoarystio <rocevo.id@gmail.com>
2022-04-25 22:30:24 +0700

    crud

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ app.get('/project/:id', function(req,res) {
         }
     })
 
-    res.render('project', {dataId: id, myProjek})
+    res.render('project', {dataId: id, myProjek});
 })
 
 app.get('/delete-projek/:id', function (req, res) {
@@ -142,20 +142,50 @@ app.get('/edit-projek/:id', function (req, res) {
     })
 
     res.render('edit-projek', { dataId: id, myProjek })
+})
 
-    let name = req.body.name
-    let desc = req.body.desc
-    let techicon = req.body.techicon
-    let image = req.body.image
-    let startDate = req.body.startdate
-    let endDate = req.body.enddate
+app.post('/edit-projek/:id', function(req,res) {
+    let id = req.params.id
+    let name = req.body.nameEdit
+    let desc = req.body.descEdit
+    let techicon = req.body.techiconEdit
+    let image = req.body.imageEdit
+    let startDate = req.body.startdateEdit
+    let endDate = req.body.enddateEdit
+
+    const dateStart = new Date (startDate);
+    const dateEnd = new Date (endDate);
+    const time = Math.abs(dateEnd - dateStart);
+    const days = Math.ceil(time / (1000 * 60 * 60 * 24));
+    const week = Math.ceil(days / 7);
+    const month = Math.ceil(week / 4); // Math.ceil(days / 30)
+
+    // if(days <= 7) {
+    //     Math.floor(days) + 'hari'
+    // } else if(days <= 30){
+    //     Math.ceil(days / 7) + 'minggu';
+    // } else{
+    //     Math.ceil(days / 4) + 'bulan'; // Math.ceil(days / 30)
+    // }
+
+    console.log(`
+    id           : ${id}
+    title        : ${name}, 
+    start        : ${startDate},
+    end          : ${endDate}
+    duration     : ${month} bulan,
+    desc         : ${desc}, 
+    technologies : ${techicon}, 
+    image        : ${image}
+    `);
 
     let project = {id, name, startDate, endDate, desc, techicon, image, month};
 
     projek.push(project);
+    projek.splice(id, 1);
 
-    res.redirect('/')
-})
+    res.redirect('/');
+});
 
 const port = 4000
 app.listen(port, function(){
